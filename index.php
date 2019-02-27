e4cd2a3455c37abaaa2dc0427043aa7504cf6163
hsshamloo <h.s.shamloo@gmail.com>
2019-02-27 11:04:17 +0100

    Firts commit

---
+++ index.php
@@ -0,0 +1,7 @@
+﻿<?php
+	session_start();
+	include_once ('shop_sc_fns.php');
+	do_html_header('www.prozhe.com مرجع دانلود رایگان پروژه های دانشجویی',get_categories());
+	display_latest();
+	do_html_footer();
+?>
